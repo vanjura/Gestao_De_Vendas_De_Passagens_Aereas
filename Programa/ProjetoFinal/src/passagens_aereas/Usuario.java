94c7f383ac1ab10777e72e1f86abd741450d60fd
vanjura <lucasvanjura1996@gmail.com>
2018-06-16 00:45:23 -0300

    Modificações no Usuário

--- Programa/ProjetoFinal/src/passagens_aereas/Usuario.java
+++ Programa/ProjetoFinal/src/passagens_aereas/Usuario.java
@@ -5,17 +5,29 @@
  */
 package passagens_aereas;
 
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
 /**
  *
  * @author JVict
  */
 public class Usuario {
+
     private int id_usuario;
     private String nome;
     private String senha;
+    private int nivel = 0;
 
-    public Usuario(String nome, String senha) {
-        
+    //construtor utilizado para setar os dados através do banco
+    public Usuario() {
+    }
+
+    //construtor utilizado para conseguir os dados do usuário através da tela de login
+    public Usuario(JTextField TextUsuario, JPasswordField PassUsuario) {
+        this.nome = TextUsuario.getText();
+        this.senha = PassUsuario.getText();
+        this.nivel = 0;
     }
 
     public int getId_usuario() {
@@ -40,5 +52,13 @@ public class Usuario {
 
     public void setSenha(String senha) {
         this.senha = senha;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
     }
 }
