8740976855f4cac78024ce9ceea506cff335fe4a
vanjura <lucasvanjura1996@gmail.com>
2018-06-19 19:29:04 -0300

    Banco

---
+++ Banco de Dados/banco_unicorn.sql
@@ -0,0 +1,132 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: bd_unicorn
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.1.32-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `aeroporto`
+--
+
+DROP TABLE IF EXISTS `aeroporto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `aeroporto` (
+  `registro` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `cidade` varchar(50) NOT NULL,
+  `estado` varchar(50) NOT NULL,
+  PRIMARY KEY (`registro`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `aeroporto`
+--
+
+LOCK TABLES `aeroporto` WRITE;
+/*!40000 ALTER TABLE `aeroporto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `aeroporto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `aviao`
+--
+
+DROP TABLE IF EXISTS `aviao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `aviao` (
+  `registro` varchar(10) NOT NULL,
+  `modelo` varchar(50) NOT NULL,
+  `qtd_assentos` int(11) NOT NULL,
+  `qtd_assentos_esp` int(11) NOT NULL,
+  PRIMARY KEY (`registro`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `aviao`
+--
+
+LOCK TABLES `aviao` WRITE;
+/*!40000 ALTER TABLE `aviao` DISABLE KEYS */;
+/*!40000 ALTER TABLE `aviao` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rota`
+--
+
+DROP TABLE IF EXISTS `rota`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `rota` (
+  `id_rota` int(11) NOT NULL,
+  `origem` int(11) NOT NULL,
+  `destino` int(11) NOT NULL,
+  `duracao` time NOT NULL,
+  `preco_c` float NOT NULL,
+  `preco_e` float NOT NULL,
+  PRIMARY KEY (`id_rota`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rota`
+--
+
+LOCK TABLES `rota` WRITE;
+/*!40000 ALTER TABLE `rota` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rota` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `voo`
+--
+
+DROP TABLE IF EXISTS `voo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `voo` (
+  `id_voo` int(11) NOT NULL AUTO_INCREMENT,
+  `viagem` int(11) NOT NULL,
+  `aviao` int(11) NOT NULL,
+  `data_saida` date NOT NULL,
+  `hora_saida` time NOT NULL,
+  `data_chegada` date NOT NULL,
+  `hora_chegada` time NOT NULL,
+  PRIMARY KEY (`id_voo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `voo`
+--
+
+LOCK TABLES `voo` WRITE;
+/*!40000 ALTER TABLE `voo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `voo` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-06-19 19:25:46
