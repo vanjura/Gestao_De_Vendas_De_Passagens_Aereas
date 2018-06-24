6633e2fd2216322a7ef75e23556d1b547ef36247
Vanjura <lucasvanjura1996@gmail.com>
2018-06-23 22:07:50 -0300

    inserir passagem

--- Programa/ProjetoFinal/src/connection/PassagemCRUD.java
+++ Programa/ProjetoFinal/src/connection/PassagemCRUD.java
@@ -5,10 +5,39 @@
  */
 package connection;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import passagens_aereas.Passagem;
+
 /**
  *
  * @author lucas
  */
 public class PassagemCRUD {
+    private Connection con = null;
+
+    public PassagemCRUD() {
+        con = Conexao.getConexao();
+    }
+    
+    public boolean inserir(Passagem passagem) {
+        PreparedStatement stmt = null;
+        String sql = "insert into passagem (tipo, origem, destino, data) VALUES(?,?,?,?)";
+        try {
+            stmt = con.prepareStatement(sql);
+            stmt.setString(1, passagem.getTipo());
+            stmt.setString(2, passagem.getOrigem());
+            stmt.setString(3, passagem.getDestino());
+            stmt.setDate(3, passagem.getData());
+            stmt.executeUpdate();
+            return true;
+        } catch (SQLException ex) {
+            System.err.println("Erro: " + ex);
+            return false;
+        } finally {
+            Conexao.fecharConexao(con, stmt);
+        }
+    }
     
 }
