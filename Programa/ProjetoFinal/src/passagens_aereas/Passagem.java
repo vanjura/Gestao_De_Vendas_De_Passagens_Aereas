51615e9ce019e14c1ecb3c5f08a5caf6a76f0c29
Vanjura <lucasvanjura1996@gmail.com>
2018-06-23 22:08:03 -0300

    objeto passagem

--- Programa/ProjetoFinal/src/passagens_aereas/Passagem.java
+++ Programa/ProjetoFinal/src/passagens_aereas/Passagem.java
@@ -5,10 +5,56 @@
  */
 package passagens_aereas;
 
+import java.sql.Date;
+
 /**
  *
  * @author lucas
  */
 public class Passagem {
-    
+    int id;
+    String tipo;
+    String origem;
+    String destino;
+    Date data;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getOrigem() {
+        return origem;
+    }
+
+    public void setOrigem(String origem) {
+        this.origem = origem;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
 }
